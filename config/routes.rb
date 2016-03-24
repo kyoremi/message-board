c72b5a591fe8f62d3bb838de21a3f7244e88acdc
kyoremi <kyoshiro.yamashita@gmail.com>
2016-03-24 17:00:25 +0000

    create message model

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
    root 'messages#index'
-
-  # The priority is based upon order of creation: first created -> highest priority.
+   resources :messages , only: [:create]
+  # The priorrake routesity is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
