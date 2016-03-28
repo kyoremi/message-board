3fb7723b6af35d985f3c18c6c0a410467a8902d5
kyoremi <kyoshiro.yamashita@gmail.com>
2016-03-28 16:58:32 +0000

    submit retrky

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,11 @@
 class MessagesController < ApplicationController
   before_action :set_message, only: [:edit, :update, :destroy]
    def index
-    @message = Message.new
-    # Messageを全て取得する。
-    @messages = Message.all
+     @message = Message.new
+     # Messageを全て取得する。
+     @messages = Message.all
    end
- def create
+   def create
     @message = Message.new(message_params)
     if @message.save
       redirect_to root_path , notice: 'メッセージを保存しました'
@@ -15,26 +15,26 @@ class MessagesController < ApplicationController
       flash.now[:alert] = "メッセージの保存に失敗しました。"
       render 'index'
     end
- end
-  def edit
-  end
-  def destroy
+   end
+   def edit
+   end
+   def destroy
     @message.destroy
     redirect_to root_path, notice: 'メッセージを削除しました'
-  end
-  def update
-    if @message.update(message_params)
+   end
+   def update
+     if @message.update(message_params)
       # 保存に成功した場合はトップページへリダイレクト
       redirect_to root_path , notice: 'メッセージを編集しました'
-    else
+     else
       # 保存に失敗した場合は編集画面へ戻す
       render 'edit'
-    end
-  end
+     end
+   end
   private
-  def message_params
-    params.require(:message).permit(:name, :body)
-  end
+   def message_params
+    params.require(:message).permit(:name, :body,:age)
+   end
    def set_message
     @message = Message.find(params[:id])
    end
--- app/models/message.rb
+++ app/models/message.rb
@@ -3,6 +3,6 @@ class Message < ActiveRecord::Base
   validates :name , length: { maximum: 20 } , presence: true
   # 内容は必須入力かつ2文字以上30文字以下
   validates :body , length: { minimum: 2, maximum: 30 } , presence: true
-  #年齢は０以上
-  validates :age , numericality: { greater_than: 0 } , presence: true
+  #年齢は０以上入力可
+  validates :age , numericality: { greater_than_or_equal_to: 0 } , presence: true
 end
